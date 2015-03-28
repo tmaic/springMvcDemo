3dacab8d1774ed8b958ab74af3d0260ede1be5f5
tmaic <tmaic@126.com>
2015-03-28 18:41:41 +0800

    后台模板 仿照H+

---
+++ com.demo.service/src/main/java/com/demo/util/DateUtil.java
@@ -0,0 +1,121 @@
+package com.demo.util;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * User: meichao
+ * Date: 14-6-30
+ * Time: 下午3:44
+ */
+public class DateUtil {
+
+    private final static Logger log = LogManager.getLogger(DateUtil.class);
+    public static final String DEFAULT_DATE_FORMATE = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    /**
+     * 根据格式得到格式化后的日期
+     *
+     * @param currDate 要格式化的日期
+     * @param format   日期格式，如yyyy-MM-dd
+     * @return Date 返回格式化后的日期，格式由参数<code>format</code>
+     *         定义，如yyyy-MM-dd，如2006-02-15
+     * @see java.text.SimpleDateFormat#parse(String)
+     */
+    public static Date getFormatDate(String currDate, String format) {
+        if (currDate == null) {
+            return null;
+        }
+        SimpleDateFormat dtFormatdB = null;
+        try {
+            dtFormatdB = new SimpleDateFormat(format);
+            return dtFormatdB.parse(currDate);
+        } catch (Exception e) {
+            dtFormatdB = new SimpleDateFormat(DATE_FORMAT);
+            try {
+                return dtFormatdB.parse(currDate);
+            } catch (Exception ex) {
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 根据格式得到格式化后的时间
+     *
+     * @param currDate 要格式化的时间
+     * @param format   时间格式，如yyyy-MM-dd HH:mm:ss
+     * @return String 返回格式化后的时间，格式由参数<code>format</code>定义，如yyyy-MM-dd
+     *         HH:mm:ss
+     * @see java.text.SimpleDateFormat#format(java.util.Date)
+     */
+    public static String getFormatDateTime(Date currDate, String format) {
+        if (currDate == null) {
+            return "";
+        }
+        SimpleDateFormat dtFormatdB = null;
+        try {
+            dtFormatdB = new SimpleDateFormat(format);
+            return dtFormatdB.format(currDate);
+        } catch (Exception e) {
+            dtFormatdB = new SimpleDateFormat(DEFAULT_DATE_FORMATE);
+            try {
+                return dtFormatdB.format(currDate);
+            } catch (Exception ex) {
+            }
+        }
+        return "";
+    }
+
+    /**
+     * 根据格式得到格式化后的日期
+     *
+     * @param currDate 要格式化的日期
+     * @param format   日期格式，如yyyy-MM-dd
+     * @return String 返回格式化后的日期，格式由参数<code>format</code>
+     *         定义，如yyyy-MM-dd，如2006-02-15
+     * @see java.text.SimpleDateFormat#format(java.util.Date)
+     */
+    public static String getFormatDate(Date currDate, String format) {
+        if (currDate == null) {
+            return "";
+        }
+        SimpleDateFormat dtFormatdB = null;
+        try {
+            dtFormatdB = new SimpleDateFormat(format);
+            return dtFormatdB.format(currDate);
+        } catch (Exception e) {
+            dtFormatdB = new SimpleDateFormat(DATE_FORMAT);
+            try {
+                return dtFormatdB.format(currDate);
+            } catch (Exception ex) {
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 得到格式化后的时间，格式为yyyy-MM-dd HH:mm:ss，如2006-02-15 15:23:45
+     *
+     * @param currDate 要格式化的时间
+     * @return String 返回格式化后的时间，默认格式为yyyy-MM-dd HH:mm:ss，如2006-02-15 15:23:45
+     * @see #getFormatDateTime(java.util.Date, String)
+     */
+    public static String getFormatDateTime(Date currDate) {
+        return getFormatDateTime(currDate, DEFAULT_DATE_FORMATE);
+    }
+
+    /**
+     * 取得当前系统时间，返回java.util.Date类型
+     *
+     * @return java.util.Date 返回服务器当前系统时间
+     * @see java.util.Date
+     */
+    public static Date getCurrDate() {
+        return new Date();
+    }
+}
