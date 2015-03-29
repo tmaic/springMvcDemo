eb0e40aa3744edf18c86c3dfa173ec5a381608c8
tmaic <tmaic@126.com>
2015-03-29 15:31:15 +0800

    后台模板 首页

---
+++ com.demo.service/src/main/java/com/demo/util/SidebarUtil.java
@@ -0,0 +1,16 @@
+package com.demo.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: meichao
+ * Date: 2015/3/29
+ * Time: 13:30
+ * Desc:
+ * To change this template use File | Settings | File Templates.
+ */
+public class SidebarUtil {
+
+
+
+
+}
